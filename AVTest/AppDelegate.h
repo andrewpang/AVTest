62b55ce8d77a62ba2b0d711e58ad95164475f1e1
Andrew Pang <andrewpang94@gmail.com>
2015-07-19 16:03:48 -0700

    Initial Commit

---
+++ AVTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVTest
+//
+//  Created by Andrew Pang on 7/19/15.
+//  Copyright (c) 2015 andrewpang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVTest
+//
+//  Created by Andrew Pang on 7/19/15.
+//  Copyright (c) 2015 andrewpang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
